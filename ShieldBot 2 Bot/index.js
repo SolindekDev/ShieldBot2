487eaf2b18c7f15027934459822c459c61a6b5d4
Solindek Developer <83218819+SolindekDev@users.noreply.github.com>
2021-06-15 16:34:21 +0200

    All

---
+++ ShieldBot 2 Bot/index.js
@@ -0,0 +1,4 @@
+const commandHandler = require('./files/handlers/command')
+const eventHandler = require('./files/handlers/event')
+const configClient = require('./files/config/client')
+const configCommand = require('./files/config/command')
