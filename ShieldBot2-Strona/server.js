db8dda4f8b145eafb223c31df2eb87e7097892b7
SolindekDev <83218819+SolindekDev@users.noreply.github.com>
2021-07-08 11:08:15 +0200

    Więcej statystyk

--- ShieldBot2-Strona/server.js
+++ ShieldBot2-Strona/server.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = new express()
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const os = require('os');
+const { mem, cpu } = require('node-os-utils');
+const ms = require('ms');
 
 app.set('view engine', 'ejs')
 
@@ -15,6 +18,27 @@ app.get('/', (req, res) => {
         members: client.guilds.cache.reduce((a, g) => a + g.memberCount, 0),
         channels: client.channels.cache.size
     })
+})
+
+app.get('/statystyki', (req, res) => {
+    var usedMemory = os.totalmem() -os.freemem(), totalMemory = os.totalmem();
+    res.render('statystyki', {
+        guilds: client.guilds.cache.size,
+        members: client.guilds.cache.reduce((a, g) => a + g.memberCount, 0),
+        channels: client.channels.cache.size,
+        procesor: cpu.model(),
+        watkiprocesora: cpu.count(),
+        systemoperacyjny: os.type,
+        ram: ((usedMemory/totalMemory) * 100).toFixed(2) + 'mb/2084mb'
+    })
+})
+
+app.get('/dodaj', (req, res) => {
+    res.redirect('https://discord.com/oauth2/authorize?client_id=831470538480025610&permissions=8&scope=bot')
+})
+
+app.get('/support', (req, res) => {
+    res.redirect('https://discord.gg/Sp2yT4HNJy')
 })
 
 app.listen(3000, () => {
